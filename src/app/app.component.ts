8e131e93459ed1b63ef5ef246a01dc63d8a4a34f
Gaurav Sharma3 <Gaurav_Sharma3@epam.com>
2018-08-21 14:01:43 +0530

    first comit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+declare var UUI: any
+
+import { Component, OnInit } from '@angular/core';
+import '../../js/uui-core.min.js'
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  
+
+
+  ngOnInit()
+  {
+    UUI.Sidebar.init( {over_content: true} );
+  
+
+  }
 }
